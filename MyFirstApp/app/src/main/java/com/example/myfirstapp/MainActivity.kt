355e49dbd6f6d9bd038d5838ce0a94273d4cee57
Romain SALVAN <romain.salvan@g-mail.fr>
2023-01-14 19:24:48 +0100

    Application 1 done

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.myfirstapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var nbClick : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "Compteur"
+
+        Toast.makeText(this, "Application lancée !", Toast.LENGTH_LONG).show()
+
+        val txtCompteur: TextView = findViewById<TextView>(R.id.compteur)
+        txtCompteur.text = "Nombre de click : $nbClick"
+
+        val btnClickMe = findViewById<Button>(R.id.clickme)
+        btnClickMe.setOnClickListener{
+            nbClick += 1
+            txtCompteur.text = "Nombre de click : $nbClick"
+            btnClickMe.text = "Clique moi plus fort !"
+        }
     }
 }
